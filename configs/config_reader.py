e4b53ad19366b5d5bad19ac75eb11a9d84b57ed9
sattor-apple <sattorapple@gmail.com>
2024-11-11 20:28:55 +0500

    Update config_reader.py

--- configs/config_reader.py
+++ configs/config_reader.py
@@ -5,6 +5,8 @@ from pydantic import SecretStr
 class Configuration(BaseSettings):
     TELEGRAM_BOT_TOKEN: SecretStr
     DATABASE_URL: SecretStr
+    ADMIN_ID: int
+    
     model_config = SettingsConfigDict(
         env_file='.env',
         env_file_encoding='utf-8'
