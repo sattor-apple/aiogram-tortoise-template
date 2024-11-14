1f47ec682a01bf24c144f23c86ced763b78ce122
sattor-apple <sattorapple@gmail.com>
2024-11-15 00:07:26 +0500

    Create users.py

---
+++ database/models/users.py
@@ -0,0 +1,15 @@
+from tortoise import fields
+from tortoise.models import Model
+
+
+class User(Model):
+    id = fields.IntField(pk=True)
+    user_id = fields.IntField(unique=True)
+    chat_type = fields.CharField(max_length=20, default='private')
+    language = fields.CharField(max_length=30, null=True)
+    user_type = fields.CharField(max_length=30, default='user')
+    created_at = fields.DatetimeField(auto_now_add=True)
+    updated_at = fields.DatetimeField(auto_now=True)
+
+    class Meta:
+        table = 'users'
