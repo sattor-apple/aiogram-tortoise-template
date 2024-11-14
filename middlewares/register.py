a7cec6ee71f7df354f4b19b7ae5facd4b936b1c6
sattor-apple <sattorapple@gmail.com>
2024-11-15 00:06:02 +0500

    Create register.py

---
+++ middlewares/register.py
@@ -0,0 +1,30 @@
+from aiogram import BaseMiddleware
+from aiogram.types import Update
+from database.models import User
+from typing import Callable, Awaitable, Dict, Any
+
+
+class CounterMiddleware(BaseMiddleware):
+    async def __call__(
+        self,
+        handler: Callable[[Update, Dict[str, Any]], Awaitable[Any]],
+        event: Update,
+        data: Dict[str, Any]
+    ) -> Any:
+        if event.message:
+            user_id = event.message.from_user.id
+            chat_type = event.message.chat.type
+        elif event.callback_query:
+            user_id = event.callback_query.from_user.id
+            chat_type = event.callback_query.message.chat.type
+        else:
+            return await handler(event, data)
+
+        await User.get_or_create(
+            user_id=user_id,
+            defaults=dict(
+                chat_type=chat_type,
+            )
+        )
+
+        return await handler(event, data)
